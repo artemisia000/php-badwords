4acb0f4ac7f38208b36f411c58d7ad50517ca521
artemisia000 <misiapierobon@gmail.com>
2022-01-05 16:18:39 +0100

    correzione

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+#Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a schermo il paragrafo e la sua lunghezza. Una parola da censurare viene passata dall'utente tramite parametro GET. Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+
+
+
+$paragrph = "Aspettati veleno dall'acqua stagnante.";
+echo strlen ($paragrph);
+
+
+#$badWord = $_GET['badword'];
+#echo 'BADWORD:' .$badWord;
+
+#$newParagraph = str_replace($badword, '***',$paragraph);
+#echo $newParagraph;
+#echo 'lunghezza:' .strlen($newParagraph);
+
+
+
+?>
+
+</body>
+</html>
